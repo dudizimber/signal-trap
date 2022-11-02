d31c9871872fe7776310d48467009b380be5ce43
Dudi <dudi@vizo.dev>
2022-11-02 09:46:12 +0000

    set trapped

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ import express from 'express';
  
 const app = express();
 
+async function setTrapped() {
+    import axios from "axios";
+
+    const options = {
+      method: 'GET',
+      url: 'https://simpledb.vercel.app/api/add/trapped/' + version,
+      headers: {userid: 'ffkx4l'}
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+}
 
 process.on('SIGINT', async () => {
     console.log('Got SIGINT. Shutting down...', version, process.env.K_REVISION);
+    await setTrapped();
     process.exit(0);
 })
 
 
 process.on('SIGTERM', async () => {
     console.log('Got SIGTERM. Shutting down...', version, process.env.K_REVISION);
+    await setTrapped();
     process.exit(0);
 })
 
@@ -21,7 +38,7 @@ app.use((req, res) => {
 })
 
 
-const version = 6;
+const version = 7;
 
 app.listen(process.env.PORT ?? 3010, () => {
     console.log('Started', version, process.env.K_REVISION);
